1eb565acd7a21b38e2b25b03674a6ed793b987b5
F93AB7X <joseph.schmitt1@fiserv.com>
2023-10-20 15:11:20 -0400

    ex5. completed

--- src/main/java/com/xpanxion/assignments/student/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student/JavaOne.java
@@ -3,14 +3,14 @@ package com.xpanxion.assignments.student;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
 public class JavaOne {
 
     //
     // Constructors
     //
 
-    public JavaOne() {}
+    public JavaOne() {
+    }
 
     //
     // Public methods
@@ -56,10 +56,12 @@ public class JavaOne {
 
         while (st.hasMoreTokens()) {
 
-            if (count % 2 == 0) System.out.println(st.nextToken().toUpperCase());
-            
-            else System.out.println(st.nextToken().toLowerCase());
-            
+            if (count % 2 == 0)
+                System.out.println(st.nextToken().toUpperCase());
+
+            else
+                System.out.println(st.nextToken().toLowerCase());
+
             count++;
         }
     }
@@ -73,14 +75,102 @@ public class JavaOne {
 
         String sb = new StringBuilder(input).reverse().toString();
 
-        if (input.equals(sb)) System.out.println("YES");
-        else System.out.println("NO");
-        
+        if (input.equals(sb))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
 
     }
 
     public void ex5() {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        int vowelCount = 0, consantCount = 0;
         System.out.println("Student 1: ex5.");
+        while (!input.equals("quit")) {
+            System.out.print("Enter in a word to see constants and vowels:");
+            input = sc.nextLine().toLowerCase();
+            if (input.equals("quit")) break;
+
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i'
+                        || input.charAt(i) == 'o' || input.charAt(i) == 'u') {
+                    vowelCount++;
+                }
+
+                switch (input.charAt(i)) {
+                    case 'b':
+                        consantCount++;
+                        break;
+                    case 'c':
+                        consantCount++;
+                        break;
+                    case 'd':
+                        consantCount++;
+                        break;
+                    case 'f':
+                        consantCount++;
+                        break;
+                    case 'g':
+                        consantCount++;
+                        break;
+                    case 'h':
+                        consantCount++;
+                        break;
+                    case 'j':
+                        consantCount++;
+                        break;
+                    case 'k':
+                        consantCount++;
+                        break;
+                    case 'l':
+                        consantCount++;
+                        break;
+                    case 'm':
+                        consantCount++;
+                        break;
+                    case 'n':
+                        consantCount++;
+                        break;
+                    case 'p':
+                        consantCount++;
+                        break;
+                    case 'q':
+                        consantCount++;
+                        break;
+                    case 'r':
+                        consantCount++;
+                        break;
+                    case 's':
+                        consantCount++;
+                        break;
+                    case 't':
+                        consantCount++;
+                        break;
+                    case 'v':
+                        consantCount++;
+                        break;
+                    case 'w':
+                        consantCount++;
+                        break;
+                    case 'x':
+                        consantCount++;
+                        break;
+                    case 'y':
+                        consantCount++;
+                        break;
+                    case 'z':
+                        consantCount++;
+                        break;
+                }
+            }
+
+            System.out.println("The number of vowels are: " + vowelCount +
+                    " and the number of constants are: " + consantCount);
+            vowelCount = 0;
+            consantCount = 0;
+        }
+
     }
 
     public void ex6() {
@@ -108,6 +198,6 @@ public class JavaOne {
     //
     public static void main(String[] args) {
         JavaOne javaOne = new JavaOne();
-        javaOne.ex4();
+        javaOne.ex5();
     }
 }
